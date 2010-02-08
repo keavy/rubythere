fbe3cacb1228643f873ec69854e38e26fa6f2319
Keavy McMinn <keavy@minimetre.com>
2010-02-08 17:25:30 +0000

    annotated models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+# Schema version: 20100208172043
+#
+# Table name: locations
+#
+#  id              :integer(4)      not null, primary key
+#  venue           :string(255)
+#  street_address  :string(255)
+#  street_address2 :string(255)
+#  city            :string(255)
+#  state           :string(255)
+#  country         :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class Location < ActiveRecord::Base
   has_many :events
   validates_presence_of :city, :message => '^Please add a city'
