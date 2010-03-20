4f0b0272380b75ef8329d4c0840e30c15f56c56f
Keavy McMinn <keavy@minimetre.com>
2010-03-20 13:23:18 +0000

    change app layout to use haml

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem 'RedCloth'
   config.gem "friendly_id", :version => ">= 2.3.0"
   config.gem 'hoptoad_notifier'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
