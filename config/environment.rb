0341278e051dc58c35c3b5281beedde18e747489
Keavy McMinn <keavy@minimetre.com>
2010-04-05 18:01:31 +0100

    Use whenever gem for cronjob to remove tweet cache

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
   config.gem 'hoptoad_notifier'
   config.gem 'haml'
   config.gem 'twitter'
+  config.gem 'whenever'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
