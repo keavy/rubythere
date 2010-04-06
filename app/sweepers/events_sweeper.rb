7a67e3d2cc5b1a8a037d7f4a6d36dde9e6c24f1c
Keavy McMinn <keavy@minimetre.com>
2010-04-06 21:24:37 +0100

    cache details for attend/speak include event in default_scope of Happening

---
+++ app/sweepers/events_sweeper.rb
@@ -0,0 +1,22 @@
+class EventsSweeper < ActionController::Caching::Sweeper
+  observe Event
+  
+  def after_create(record)
+    expire_cache(record)
+  end
+  
+  def after_update(record)
+    expire_cache(record)
+  end
+  
+  def after_destroy(record)
+    expire_cache(record)
+  end
+  
+  private
+  def expire_cache(record)
+    expire_page "index"
+    expire_fragment 'happenings/attend'
+    expire_fragment 'happenings/speak'
+  end
+end
