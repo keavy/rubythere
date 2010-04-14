64b631d009bf1a73f27a0da41af9aacf64fc01d4
Keavy McMinn <keavy@minimetre.com>
2010-04-14 11:10:32 +0100

    require twitter gem

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml'
   config.gem 'twitter'
   config.gem 'whenever'
+  config.gem 'twitter', :version => ">= 0.9.0"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
