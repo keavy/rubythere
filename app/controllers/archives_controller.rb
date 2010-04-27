606bb299dde3533d1f64d571ce9651fd5f8e0e76
Keavy McMinn <keavy@minimetre.com>
2010-04-27 13:12:18 +0100

    set layout for archive

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,4 +1,6 @@
 class ArchivesController < ApplicationController
+  layout 'listings'
+  
   def show
     @happenings = Happening.approved.past + Happening.approved.unknown unless fragment_exist?('events/archive')
   end
