29ff5149dd02739d2bfe90e79b687b4467e8a887
Keavy McMinn <keavy@minimetre.com>
2010-06-16 13:47:10 +0100

    ical for events

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -17,3 +17,7 @@ Given /^twitter has some search results for "([^\"]*)"$/ do |name|
   stub_get("http://search.twitter.com/search.json?q=#{CGI.escape(name)}", 'search.json')
 end
 
+Then /^I should have downloaded a file called "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
