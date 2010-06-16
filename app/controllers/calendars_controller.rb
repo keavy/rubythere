3239c4c5c6a27d65168b6892ee08df48b93ca488
Keavy McMinn <keavy@minimetre.com>
2010-06-16 13:51:32 +0100

    Just date, not times, for calendar events

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -6,8 +6,8 @@ class CalendarsController < ApplicationController
     @happenings = Happening.approved.upcoming
     @happenings.each do |happening|
       @cal.event do
-        dtstart       happening.start_at.strftime("%Y%m%dT%H%M%S")
-        dtend         happening.end_at.strftime("%Y%m%dT%H%M%S") if happening.end_at
+        dtstart       happening.start_at.strftime("%Y%m%d")
+        dtend         happening.end_at.strftime("%Y%m%d") if happening.end_at
         summary       happening.event.name
         description   happening.description
         url           happening.url
