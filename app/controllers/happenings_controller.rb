4ab8211d1c4da821fe26a1961c2959ba32a05822
Keavy McMinn <keavy@minimetre.com>
2011-01-06 20:06:15 +0100

    Reverse order of happenings in the index

--- app/controllers/happenings_controller.rb
+++ app/controllers/happenings_controller.rb
@@ -11,7 +11,7 @@ class HappeningsController < ApplicationController
     if @event.happenings.blank?
       @happening  = @event.happenings.build
     else
-      @happenings = @event.happenings
+      @happenings = @event.happenings.reverse
     end
   end
   
