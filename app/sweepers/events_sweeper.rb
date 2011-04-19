1475eb62fdeee1b568ae5ace91a3a156ae89b448
Keavy McMinn <keavy@minimetre.com>
2011-04-19 17:33:59 +0100

    events cache sweeper: observe Happening too

--- app/sweepers/events_sweeper.rb
+++ app/sweepers/events_sweeper.rb
@@ -1,18 +1,19 @@
 class EventsSweeper < ActionController::Caching::Sweeper
   observe Event
-  
+  observe Happening
+
   def after_create(record)
     expire_cache(record)
   end
-  
+
   def after_update(record)
     expire_cache(record)
   end
-  
+
   def after_destroy(record)
     expire_cache(record)
   end
-  
+
   private
   def expire_cache(record)
     expire_fragment 'events/attend'
