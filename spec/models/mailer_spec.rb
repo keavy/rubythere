8958b1bc5ff198e6d6711be5a8fa7d930aa13d04
Keavy McMinn <keavy@minimetre.com>
2011-04-24 21:48:58 +0100

    Mailer spec, replacing old mailer unit test

--- spec/models/mailer_spec.rb
+++ spec/models/mailer_spec.rb
@@ -23,4 +23,23 @@ describe Mailer do
 
     it_should_behave_like "rubythere.com email"
   end
+
+  describe "when sending a new user notification" do
+    before(:each) do
+      @user  = Factory(:user)
+      @email = Mailer.new_user_notification(@user)
+    end
+
+    it "should contain the user's screen name" do
+      @email.body.should =~ /#{@user.screen_name}/
+    end
+
+    it "should be sent to the EMAIL_CONTACT address" do
+      @email.to.should == ["#{EMAIL_CONTACT}"]
+    end
+
+    it "should be sent from the EMAIL_CONTACT address" do
+      @email.from.should == ["#{EMAIL_CONTACT}"]
+    end
+  end
 end
