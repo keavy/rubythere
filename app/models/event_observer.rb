17657540d12f8255cf4d56b53f08bcf2740a59a3
Keavy McMinn <keavy@minimetre.com>
2011-05-13 11:51:35 -0700

    simplify event notification

--- app/models/event_observer.rb
+++ app/models/event_observer.rb
@@ -1,5 +1,5 @@
 class EventObserver < ActiveRecord::Observer
   def after_create(event)
-    Mailer.new_event_notification(event).deliver unless event.admin_submitted || !event.submitter.present?
+    Mailer.new_event_notification(event).deliver unless !event.submitter.present?
   end
 end
