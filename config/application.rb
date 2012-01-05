6d86a43c502222a985c4c2dffb321b0d3206d1a6
Keavy McMinn <keavy@minimetre.com>
2012-01-05 21:54:50 +0000

    fix app_config load

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+APP_CONFIG = YAML.load(File.read(File.expand_path('../app_config.yml', __FILE__)))[Rails.env].symbolize_keys
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
