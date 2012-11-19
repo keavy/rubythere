400fda7d5a3163ec5b5e257b4a0994cfc579bf95
Keavy McMinn <keavy@github.com>
2012-11-19 22:47:01 +0000

    updating Rails stuff

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,9 @@ require 'rails/all'
 require 'dotenv'
 Dotenv.load
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -30,24 +26,35 @@ module Rubythere
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
     # Activate observers that should always be running.
-    config.active_record.observers = :event_observer, :user_observer
+    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'UTC'
+    # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
+
+    # Use SQL instead of Active Record's schema dumper when creating the database.
+    # This is necessary if your schema can't be completely dumped by the schema dumper,
+    # like if you have constraints or database-specific column types
+    # config.active_record.schema_format = :sql
+
+    # Enforce whitelist mode for mass assignment.
+    # This will create an empty whitelist of attributes available for mass-assignment for all models
+    # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
+    # parameters by using an attr_accessible or attr_protected declaration.
+    config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
     config.assets.enabled = true
@@ -61,6 +68,3 @@ EMAIL_CONTACT = 'mail@rubythere.com'
 
 ActionController::Base.cache_store = :file_store, "#{Rails.root}/tmp/cache"
 ActionController::Base.page_cache_directory = "#{Rails.root}/tmp/cache"
-
-# Haml::Template.options[:format] = :html5
-# Haml::Template.options[:escape_html] = false
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rubythere::Application.config.secret_token = '16e1c62dbb61ed0761d1f9c3d3b7c7dc79d38068ec78fc087ce3c0015ab92945ef2796fe1da6d15b03a5193c9918b28abf769374cc2ab4911541831fc3794e2f'
+Rubythere::Application.config.secret_token = '687eb23250a0255c6fba986998c3ddc07c04e786ea246c83874ece54f11ef96e09c075e0f8a69dee2cfc47cc896e8da6e0f749e61634f40c7c565bf9ebda1d1b'
