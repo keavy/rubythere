867ebc681e50858178474be163826db68fa2cacd
Keavy McMinn <keavy@github.com>
2012-11-22 21:20:45 +0000

    assets

--- config/application.rb
+++ config/application.rb
@@ -63,6 +63,9 @@ module Rubythere
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # If you are deploying Rails 3.1 on Heroku, you may want to set:
+    config.assets.initialize_on_precompile = false
   end
 end
 
